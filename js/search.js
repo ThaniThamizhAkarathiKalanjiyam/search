50228d8406ba8f5160e4239b448a6b26354b3f44
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2022-08-30 18:35:50 +0530

    ghhj

--- js/search.js
+++ js/search.js
@@ -2,11 +2,11 @@ $(document).ready(function(){
 
  wordsearch = function (funcData) {
 	 
-		var searctTextVal = $("#txtSearch").val()
-		debugger
+		var searctTextVal = $("#txtSearch").val().toLowerCase()
+		
 		
         $.ajax({
-            url: 'data/'+searctTextVal+'.md',
+            url: 'https://effervescent-babka-d3abb0.netlify.app/data/'+searctTextVal,
             type: 'GET',
             //dataType: 'json',
             data: {},
@@ -26,8 +26,40 @@ $(document).ready(function(){
                 $("#meanings").html("This word is not available at TTAK. Pls add at request page.")
             }
         });
-    }
+    
+		updateSearchWords(searctTextVal)
+	}
 
+ updateSearchWords = function (txtsearchLow) {
+		 
+		 if(txtsearchLow !== undefined){
+			 
+		 var utf_txtsearchLow = decodeURIComponent(txtsearchLow);
+		 
+        $.ajax({
+            url: 'https://docs.google.com/forms/d/e/1FAIpQLSfHQVgPZRpRq2Fegi9LFoibfNtCjLzufxCdqQYqlhL81VnkVA/formResponse',
+            type: 'POST',
+            dataType: 'jsonp',
+            data: {
+				"entry.891892085":utf_txtsearchLow,
+				"entry.1113699608":utf_txtsearchLow,
+				"submit":"Submit",
+				"origin":"*"
+			},
+            success: function (ResponseJsonE) {
+                
+            },
+            error: function () {
+                
+            }
+        });
+    
+		 }
+	
+	};
+	
+	
+	
 $("#btnSearch").click(function(){
 
 wordsearch()
