93b38ee796bb13b66884db9c2107c093fea07111
mpm2050 <pitchaimbox-travel@yahoo.co.in>
2022-09-09 09:25:49 +0530

    fdgfdgf

--- js/search.js
+++ js/search.js
@@ -2,27 +2,57 @@ $(document).ready(function(){
 
  wordsearch = function (funcData) {
 	 
+	 $.getJSON('https://effervescent-babka-d3abb0.netlify.app/json/அகஞ்சேரனு', function(data) {
+    console.log(data);
+});
+	 
 		var searctTextVal = $.trim($("#txtSearch").val().toLowerCase())
 		
 		
         $.ajax({
-            url: 'data/'+searctTextVal,//+".md",
+            //url: 'data/'+searctTextVal,//+".md",
+			url: 'https://effervescent-babka-d3abb0.netlify.app/json/'+searctTextVal,//+".md",
             type: 'GET',
-            //dataType: 'json',
+            dataType: 'jsonp',
             data: {},
-            success: function (ResponseJsonE) {
+			headers:{
+				"Access-Control-Allow-Origin": '*'
 				
-				var converter = new showdown.Converter(),
-					text      = ResponseJsonE,
-					html      = converter.makeHtml(text);
+			},
+            success: function (ResponseJsonE) {
 
-                if (ResponseJsonE != null) {
-                    $("#meanings").html(html)
-                } else {
+var accordionParent = $("<div>")
+$(accordionParent).addClass("accordion");
+	var accordionItem = $("<div>")
+	$(accordionItem).addClass("accordion-item");
+		var accordionHeader = $("<h2>")
+		$(accordionHeader).addClass("accordion-header");
+			var accordionButton = $("<button>")
+			$(accordionButton).addClass("accordion-button");
+			$(accordionButton).html("Accordion Item #1");
+			$(accordionHeader).html(accordionButton)
+		$(accordionItem).html(accordionHeader)
+		var accordionCollapse = $("<div>")
+		$(accordionCollapse).addClass("accordion-collapse collapse show");
+			var accordionBody = $("<div>")
+			$(accordionBody).addClass("accordion-body");
+			$(accordionBody).html("<strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.")
+		$(accordionItem).html(accordionCollapse)
+		debugger
+		  $("#meanings").html(accordionParent)
+				
+				// var converter = new showdown.Converter(),
+					// text      = ResponseJsonE,
+					// html      = converter.makeHtml(text);
+
+                // if (ResponseJsonE != null) {
+                    // $("#meanings").html(html)
+                // } else {
                     
-                }
+                // }
             },
-            error: function () {
+            error: function (data) {
+				debugger
                 $("#meanings").html("This word is not available at TTAK. Pls add at request page.")
             }
         });
